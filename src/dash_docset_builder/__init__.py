6d321d66c2adfdf0d2ebfa9747303e09a15c87f7
Louie Shprung <lshprung@tutanota.com>
2025-12-09 21:56:24 -0500

    Combine create_table and insert into a class

--- src/dash_docset_builder/__init__.py
+++ src/dash_docset_builder/__init__.py
@@ -1,21 +1,21 @@
 __all__ = [
+        'DB',
         'DocsetSkeleton',
-        'create_table',
         'get_title',
         'get_argparse_template',
         'Gnu_Index_Terms',
-        'insert',
         'Stylesheet_Setter'
 ]
 
 from .argparse_template import get_argparse_template
+from .db import DB
 from .docset_skeleton import DocsetSkeleton
-from .create_table import create_table
 from .get_title import get_title
 from .gnu import Gnu_Index_Terms
-from .insert import insert
 from .set_stylesheet import Stylesheet_Setter
 
 # FIXME terrible practice, need to find a better way to hide these
+del db
+del docset_skeleton
 del gnu
 del set_stylesheet
